b23a5c0e055d8692043f4d7c2053351df718d9fe
Mei <ymei@cslab05.cs.skidmore.edu>
2014-09-03 20:06:17 -0400

    Initital commit.

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author ymei
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("-----");
+		System.out.println("        O");
+		System.out.println("        +");
+		System.out.println("     ------");
+		System.out.println("----");
+		System.out.println("-----   ----");
+		System.out.println("---------- -");
+		System.out.println("\"Hello World!\"I said");
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
