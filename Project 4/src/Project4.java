3e1d0b02011a72c982a84d92399e4fa9c42470f8
TengGe <ymei@skidmore.edu>
2014-10-09 14:16:10 -0400

    Update Project4.java

--- Project 4/src/Project4.java
+++ Project 4/src/Project4.java
@@ -18,66 +18,64 @@ public class Project4 {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(new File("src/input.txt"));
-		input.next();
-		String one=input.next();
-		if (one.equals("Checking")) {
-			checkingAmount(input);
-}          
-		else if (one.equals("Saving")){
-			savingAmount(input);
-		}
-		else{
-			creditAmount(input);
-			
-		}
-		
-	}
-	public static void checkingAmount(Scanner input){
-		String two=input.next();
-		double checkingBalance=0.0;
-		if(two.equals("Deposit")){
-			checkingBalance+=input.nextDouble();
-		}
-		else if(two.equals("Withdrawal")){
-			checkingBalance-=input.nextDouble();
-			
-		}
-		else{
+
+		while (input.hasNext()) {
 			input.next();
-			checkingBalance+=input.nextDouble();
+			String one=input.next();
+			if (one.equals("Checking")) {
+				checkingAmount(input);
+			} else if (one.equals("Saving")) {
+				savingAmount(input);
+			} else {
+				creditAmount(input);
+
+			}
+		}
+
+	}
+
+	public static void checkingAmount(Scanner input) {
+		String two = input.next();
+		double checkingBalance = 0.0;
+		if (two.equals("Deposit")) {
+			checkingBalance += input.nextDouble();
+		} else if (two.equals("Withdrawal")) {
+			checkingBalance -= input.nextDouble();
+
+		} else {
+            input.next();
+			checkingBalance += input.nextDouble();
 		}
 		System.out.println(checkingBalance);
 	}
-	public static void savingAmount(Scanner input){
-		String two=input.next();
-		double savingBalance=0.0;
-		if(two.equals("Deposit")){
-			savingBalance+=input.nextDouble();
-		}
-		else if(two.equals("Withdrawal")){
-			savingBalance-=input.nextDouble();
-			
-		}
-		else{
-			input.next();
-			savingBalance+=input.nextDouble();
+
+	public static void savingAmount(Scanner input) {
+		String two = input.next();
+		double savingBalance = 0.0;
+		if (two.equals("Deposit")) {
+			savingBalance += input.nextDouble();
+		} else if (two.equals("Withdrawal")) {
+			savingBalance -= input.nextDouble();
+
+		} else {
+            input.next();
+			savingBalance += input.nextDouble();
 		}
 		System.out.println(savingBalance);
 	}
-	public static void creditAmount(Scanner input){
-		String two=input.next();
-		double creditBalance=0.0;
-		if(two.equals("Deposit")){
-			creditBalance+=input.nextDouble();
-		}
-		else if(two.equals("Withdrawal")){
-			creditBalance-=input.nextDouble();
-			
-		}
-		else{
-			input.next();
-			input.next();
-			creditBalance+=input.nextDouble();
+
+	public static void creditAmount(Scanner input) {
+		String two = input.next();
+		double creditBalance = 0.0;
+		if (two.equals("Deposit")) {
+			creditBalance += input.nextDouble();
+		} else if (two.equals("Withdrawal")) {
+			creditBalance -= input.nextDouble();
+
+		} else {
+            input.next();
+            input.next();
+			creditBalance += input.nextDouble();
 		}
 		System.out.println(creditBalance);
 	}
