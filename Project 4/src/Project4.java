c3a4d93c9ac08587577c96d1499900a13821560e
Mei <ymei@cslab05.cs.skidmore.edu>
2014-10-08 20:28:17 -0400

    Added Project 4

---
+++ Project 4/src/Project4.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Yuteng Mei
+
+ *
+ */
+import java.util.*;
+import java.io.*;
+
+public class Project4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(new File("src/input.txt"));
+		while (input.hasNextLine()) {
+			input.next();
+	
