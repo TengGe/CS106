0eaab4c9c580fd8e89aaa8e399ff70083c3800e2
TengGe <ymei@skidmore.edu>
2014-11-03 17:21:04 -0500

    commit
    "Updated Project 5."

--- Project 5/src/Calculate.java
+++ Project 5/src/Calculate.java
@@ -2,8 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
-
 /**
  * 
  */
@@ -12,68 +10,83 @@ import java.util.Scanner;
  * @author yuten_000
  *
  */
+
+// A class that analyze the input file
 public class Calculate {
-	
-	public double allDegree=0.0;
-	public double maxValue=0.0;
-    public double minValue=1000.0;
-	
-	
+
+	// Declare the public variable named allDegree and store 0.0 value.
+	public double allDegree = 0.0;
+	// Declare the public variable named maxValue and store 0.0 value.
+	public double maxValue = 0.0;
+	// Declare the public variable named minValue and store 1000.0 value.
+	public double minValue = 1000.0;
+
+	// Create a Data array object named data.
+	Data[] data;
+	// Create a Scanner object called input.
 	Scanner input;
-	
-	public Calculate() throws FileNotFoundException{
-		
-		 this.input = new Scanner(new File("src/input.txt"));
+
+	/**
+	 * Create a constructor to read the input file.
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public Calculate() throws FileNotFoundException {
+
+		this.input = new Scanner(new File("src/input.txt"));
+		// Create a Store object named x
+		Store x = new Store();
+		// Call a getData method in the Store object and set it to the Data
+		// array object called data.
+		this.data = x.getData();
 	}
 
-
-
-
-
-	public double getAverage() throws FileNotFoundException{
-		Lines l=new Lines();
-		Store x=new Store();
-		for (Data d: x.getData()){
-		 this.allDegree+=d.getDegree();
+	/**
+	 * A method that calculate the average degree of the input file
+	 * 
+	 * @return The average degree
+	 * @throws FileNotFoundException
+	 */
+	public double getAverage() throws FileNotFoundException {
+		for (Data d : this.data) {
+			this.allDegree += d.getDegree();
 		}
-		return this.allDegree/l.getLines();
+		return this.allDegree / this.data.length;
 	}
-	
-	public double getMax() throws FileNotFoundException{
-		
-		 while (input.hasNext()){
-			 String date=input.next();
-			 String dayOfWeek=input.next();
-			 double degree=input.nextDouble();
-			 
-			 if (degree>maxValue){
-				 maxValue=degree;
-			 }
-			 
-			
-		
+
+	/**
+	 * A method that calculate the maximum degree of the input file
+	 * 
+	 * @return Maximum degree
+	 * @throws FileNotFoundException
+	 */
+
+	public double getMax() throws FileNotFoundException {
+
+		for (Data d : this.data) {
+			if (d.degree > maxValue) {
+				maxValue = d.degree;
+			}
+		}
+		return maxValue;
 	}
-		 return maxValue;
+
+	/**
+	 * A method that calculate the minimum degree of the input file
+	 * 
+	 * @return Minimum degree
+	 * @throws FileNotFoundException
+	 */
+	public double getMin() throws FileNotFoundException {
+
+		for (Data d : this.data) {
+			if (d.degree < minValue) {
+				minValue = d.degree;
+			}
+
+		}
+		return minValue;
+
 	}
-	
-	public double getMin() throws FileNotFoundException{
-		
-		 while (input.hasNext()){
-			 String date=input.next();
-			 String dayOfWeek=input.next();
-			 double degree=input.nextDouble();
-			
-			 if (degree<minValue){
-				 minValue=degree;
-			 }
-			 
-			
-		
-	}
-		 return minValue;
-	}
-	
 
 }
-
-
--- Project 5/src/Lines.java
+++ Project 5/src/Lines.java
@@ -9,25 +9,37 @@
 import java.io.*;
 import java.util.*;
 
-
+// A class that computes the total lines of the input file.
 public class Lines {
-	
+
+	// Create a scanner object called input
 	Scanner input;
-	
-	public Lines() throws FileNotFoundException{
-		
-		 this.input = new Scanner(new File("src/input.txt"));
+
+	/**
+	 * Read the file called input
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public Lines() throws FileNotFoundException {
+
+		this.input = new Scanner(new File("src/input.txt"));
 	}
-	
-	public int getLines(){
+
+	/**
+	 * A method that calculate the total lines of input file and return the
+	 * value.
+	 * 
+	 * @return the numbers of lines of input file.
+	 */
+	public int getLines() {
 		int numLines = 0;
-	
+
 		while (input.hasNextLine()) {
 			input.nextLine();
 			numLines++;
+		}
+
+		return numLines;
 	}
-		
-	return numLines;
-	}
-	
+
 }
